d60a487b8432ba40ed7b4db4366b7559c304dbe7
Bulat Kidasov <kidasov@mercurydevelopment.com>
2024-11-14 01:46:39 +0400

    Refactor code

--- ios/BarcodeCameraManager.swift
+++ ios/BarcodeCameraManager.swift
@@ -6,23 +6,16 @@ class BarcodeCameraManager: NSObject, AVCaptureVideoDataOutputSampleBufferDelega
     // MARK: - Properties
     private var backCamera: AVCaptureDevice?
     private var captureVideoOutput: AVCaptureVideoDataOutput?
+    private let captureSession = AVCaptureSession()
     
     var onCaptureOutput: (_ sampleBuffer: CMSampleBuffer) -> Void
     var onShowError: (_ title: String, _ message: String) -> Void
     
     lazy var cameraPreviewLayer: AVCaptureVideoPreviewLayer = {
-        let layer = AVCaptureVideoPreviewLayer(session: captureSession)
-        layer.videoGravity = .resizeAspectFill
-        return layer
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspectFill
+        return previewLayer
     }()
-    
-    lazy var captureSession: AVCaptureSession = {
-        let session = AVCaptureSession()
-        session.sessionPreset = .hd1920x1080
-        return session
-    }()
-    
-    private let captureDevice: AVCaptureDevice? = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
     
     // MARK: - Initializer
     init(onCaptureOutput: @escaping (_ sampleBuffer: CMSampleBuffer) -> Void, onShowError: @escaping (_ title: String, _ message: String) -> Void) {
@@ -34,65 +27,55 @@ class BarcodeCameraManager: NSObject, AVCaptureVideoDataOutputSampleBufferDelega
     
     // MARK: - Private Methods
     private func setupCamera() {
-        configureCaptureDevice()
-        setupBackCamera()
-        setupCaptureSession()
+        captureSession.sessionPreset = .hd1920x1080
+        configureBackCamera()
+        configureCaptureSessionInput()
+        configureCaptureSessionOutput()
         startCaptureSession()
     }
     
-    private func configureCaptureDevice() {
-        guard let captureDevice = captureDevice else {
-            showError("Error", "Unable to access capture device.")
-            return
-        }
-        
-        do {
-            try captureDevice.lockForConfiguration()
-            captureDevice.autoFocusRangeRestriction = .near
-            captureDevice.unlockForConfiguration()
-        } catch {
-            showError("Error", "Can't set camera focus restriction.")
-            return
-        }
-    }
-    
-    private func setupBackCamera() {
-        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(
+    private func configureBackCamera() {
+        let discoverySession = AVCaptureDevice.DiscoverySession(
             deviceTypes: [.builtInWideAngleCamera],
             mediaType: .video,
             position: .back
         )
         
-        backCamera = deviceDiscoverySession.devices.first { $0.position == .back }
-        
-        guard backCamera != nil else {
-            showError("Error", "There seems to be no camera on your device.")
-            return
+        if let camera = discoverySession.devices.first {
+            backCamera = camera
+        } else {
+            showError("Error", "No back camera available on this device.")
         }
     }
     
-    private func setupCaptureSession() {
+    private func configureCaptureSessionInput() {
         guard let backCamera = backCamera else { return }
         
         do {
-            let captureDeviceInput = try AVCaptureDeviceInput(device: backCamera)
-            captureSession.addInput(captureDeviceInput)
+            try backCamera.lockForConfiguration()
+            backCamera.autoFocusRangeRestriction = .none
+            backCamera.unlockForConfiguration()
+            
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            captureSession.addInput(input)
         } catch {
-            showError("Error", "Your camera can't be used as an input device.")
-            return
+            showError("Error", "Unable to set up back camera input.")
         }
-        
-        captureVideoOutput = AVCaptureVideoDataOutput()
-        captureVideoOutput?.videoSettings = [
+    }
+    
+    private func configureCaptureSessionOutput() {
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.videoSettings = [
             kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: kCVPixelFormatType_32BGRA)
         ]
-        captureVideoOutput?.alwaysDiscardsLateVideoFrames = true
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: .userInteractive))
         
-        let sampleBufferQueue = DispatchQueue.global(qos: .userInteractive)
-        captureVideoOutput?.setSampleBufferDelegate(self, queue: sampleBufferQueue)
-        
-        if let captureVideoOutput = captureVideoOutput {
-            captureSession.addOutput(captureVideoOutput)
+        if captureSession.canAddOutput(videoOutput) {
+            captureSession.addOutput(videoOutput)
+            captureVideoOutput = videoOutput
+        } else {
+            showError("Error", "Unable to add video output.")
         }
     }
     
@@ -102,7 +85,7 @@ class BarcodeCameraManager: NSObject, AVCaptureVideoDataOutputSampleBufferDelega
     
     private func showError(_ title: String, _ message: String) {
         print("\(title): \(message)")
-        self.onShowError(title, message)
+        onShowError(title, message)
     }
     
     // MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
